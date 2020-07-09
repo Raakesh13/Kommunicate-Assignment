9616daddb8d6b37ca06a43d0fb45334e5ee95775
rocky13 <rakesh.cse7@gmail.com>
2020-07-10 02:51:02 +0530

    added screenshot and updated the python version to all in pipfile and pipfile.lock

--- resources/MoviesToWorkOn.py
+++ resources/MoviesToWorkOn.py
@@ -3,10 +3,10 @@ from flask_restful import Resource
 from .utils import maximizeProfit
 
 class Movies(Resource):
+
     #Post request to get the movie list from an actor and return the maximum profit and list of movies the actor should work on
     def post(self): 
-        body = request.get_json(force=True)
+        body = request.get_json(force=True)        
         movies = body['moviesList']
         res = maximizeProfit(movies)
-
         return make_response(jsonify(res), 200)
